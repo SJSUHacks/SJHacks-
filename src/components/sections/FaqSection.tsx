906f4a07e4dcad601d96bb74a8a48856af23ad9c
Anishka Chauhan <email.friendme@gmail.com>
2026-04-08 00:06:29 -0700

    sponsors section, tracks page, animations

--- src/components/sections/FaqSection.tsx
+++ src/components/sections/FaqSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Placeholder from "../Placeholder";
 
 const FaqSection: React.FC = () => {
   return (
--- src/components/sections/TracksSection.tsx
+++ src/components/sections/TracksSection.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Placeholder from "../Placeholder";
 
 const TracksSection: React.FC = () => {
   return (
